0cfe0e3383e8eddb356db9c9d46d3db698d94a58
lubat <lubatatur58@gmail.com>
2022-07-27 13:18:24 +0300

    star animation

--- src/components/star/star.tsx
+++ src/components/star/star.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import starPic from "../../images/star.png";
 import './star.scss'
 
 const Star = () => {
+    const starAnimationDuration = 750;
+    const [isAnimated, setIsAnimated] = useState('')
+    function toggleIsAnimated(){
+        setIsAnimated(prev=>!prev?'animated':'')
+    }
+    function startStarAnimation(){
+        toggleIsAnimated()
+        setTimeout(()=>toggleIsAnimated(),starAnimationDuration)
+    }
+
+
     return (
-        <img src={starPic} alt="star" className='animated' id='star'/>
+        <img src={starPic} alt="star" onMouseEnter={()=> {
+            startStarAnimation()
+        }} className={isAnimated} id='star'/>
     );
 };
 
